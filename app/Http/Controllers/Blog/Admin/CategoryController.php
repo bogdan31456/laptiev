dafe2e5f7e3d549681fb43c5d50875dd08ed356c
bogdan31456 <bogdan.laptyev@gmail.com>
2021-04-19 20:47:39 +0300

    9lab

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -61,9 +61,8 @@ class CategoryController extends BaseController
 
     {
         $data = $request->input(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
+
+
 
         $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
 
